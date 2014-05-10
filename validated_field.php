967f44764dc963f277163b469702465b74f1c1a1
doublesharp <doublesharp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-10 01:01:11 +0000

    = 1.2.5.1 = * Remove debug log statement
    git-svn-id: https://plugins.svn.wordpress.org/validated-field-for-acf/trunk@911434 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- validated_field.php
+++ validated_field.php
@@ -4,7 +4,7 @@ Plugin Name: Advanced Custom Fields: Validated Field
 Plugin URI: http://www.doublesharp.com/
 Description: Server side validation, input masking and more for Advanced Custom Fields
 Author: Justin Silver
-Version: 1.2.5
+Version: 1.2.5.1
 Author URI: http://doublesharp.com/
 */
 
@@ -13,7 +13,7 @@ if (!function_exists("register_acf_validated_field")):
 function register_acf_validated_field(){
 
 	if ( !defined('ACF_VF_VERSION') )
-		define('ACF_VF_VERSION', '1.2.5');
+		define('ACF_VF_VERSION', '1.2.5.1');
 
 	if ( !defined('ACF_VF_PLUGIN_FILE') )
 		define('ACF_VF_PLUGIN_FILE', __FILE__);
@@ -25,7 +25,6 @@ function register_acf_validated_field(){
 add_action( 'acf/register_fields', 'register_acf_validated_field' );
 
 function load_textdomain_acf_vf() {
-	error_log("trying");
 	load_plugin_textdomain( 'acf_vf', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 }
 add_action( 'plugins_loaded', 'load_textdomain_acf_vf' );
